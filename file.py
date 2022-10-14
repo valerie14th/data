095ba63ce14969e652482e0bdc6bbd195829835f
valerie14th <115797676+valerie14th@users.noreply.github.com>
2022-10-14 20:00:20 +0700

    Create file.py
    this is a greeting file from python

---
+++ file.py
@@ -0,0 +1,11 @@
+# Hello Mate, this is my new GitHub Account
+# I love python programming languange
+# So, i will try my first python in this GitHub
+
+nama = str(input("who is your name?\n-"))
+umur = int(input("how old are you?\n-"))
+hobby = input("what is your hobby?\n-")
+
+greeting = str(input("are you a robot?\n-"))
+if greeting == "no" or "n" or "nah":
+    print(f"ouh ok, nice to meet you {nama}")
